07d04cfe46647e653326f99ff0b2c7469b1d83ed
brendena <cerebralasylum1@gmail.com>
2017-01-06 17:28:36 -0500

    added to readme

--- makeTensorPickle/convertMp3ToWav.sh
+++ makeTensorPickle/convertMp3ToWav.sh
@@ -2,6 +2,7 @@
 #removing extension http://unix.stackexchange.com/questions/180271/remove-a-specific-extension-from-all-the-files-in-a-directory
 
 #this will convert all mp3 into wav and delete the mp3 files
+
 for file in *.mp3; do
     filename="${file%.*}"
     wavextension=".wav"
